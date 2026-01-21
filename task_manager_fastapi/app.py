8ad69b52e8ea8ba36837a28c69eb48d03bafca77
Victor Brunner <vitorandreymartinsbrunner@hotmail.com>
2026-01-21 20:37:17 -0300

    Primeiro commit do projeto TASK MANAGER criado em FastAPI

---
+++ task_manager_fastapi/app.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Task Manager Fast API - First function
+@app.get(path="/", status_code=200)
+def read_root():
+    return {"message": "Hello World!"}
+
