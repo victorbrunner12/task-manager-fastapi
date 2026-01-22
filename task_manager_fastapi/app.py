7555ba3d0ff8effd3828c9a084ed2070087585e4
Victor Brunner <vitorandreymartinsbrunner@hotmail.com>
2026-01-22 00:00:55 -0300

    Adicionando configuração do ruff com lint e format

--- task_manager_fastapi/app.py
+++ task_manager_fastapi/app.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 # Task Manager Fast API - First function
 @app.get(path="/", status_code=200)
 def read_root():
     return {"message": "Hello World!"}
-
